e8a9275b0d77189e59f141e3a0fd0074c1257d4f
SheikhULAB <tosibalaptop86@gmail.com>
2024-07-02 16:01:26 +0600

    varable naming

---
+++ variables/variable_naming.py
@@ -0,0 +1,14 @@
+user_name = input("What is your name")
+
+#Legal variable names:
+myvar = "yamin"
+my_var = "yamin"
+_my_var = "yamin"
+myVar = "yamin"
+MYVAR = "yamin"
+myvar2 = "yamin"
+
+#Illegal variable names:
+# 2myvar = "John"
+# my-var = "John"
+# my var = "John"
