af0ca89be329d51a727454386c6531e55c09b04c
SheikhULAB <tosibalaptop86@gmail.com>
2024-07-04 14:34:27 +0600

    types

---
+++ data_types/types.py
@@ -0,0 +1,12 @@
+num_char = len(input("name\n"))
+
+new_num_char = str(num_char)
+
+print("Your name has " + new_num_char + " characters.")
+
+
+a = 123
+print(type(a))
+
+b = str(123)
+print(type(b))
