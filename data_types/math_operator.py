9e898caec1486e0051973dd75bc86d93efbad8b8
SheikhULAB <tosibalaptop86@gmail.com>
2024-07-08 10:11:09 +0600

    math operator

---
+++ data_types/math_operator.py
@@ -0,0 +1,13 @@
+3 + 5
+7 - 4
+3 * 2
+print(type(6 / 3))
+print(2 ** 3)
+
+# PEMDASLR
+# Parentheses ()
+# Exponents **
+# Multiplication *         # Division /              equal important left to right
+# Addition +               # Subtraction -           equal important left to right
+
+print(3 * 3 + 3 / 3 - 3)
