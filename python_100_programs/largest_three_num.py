5d3db2653be4d6e85635356251728be23ad3621c
SheikhULAB <tosibalaptop86@gmail.com>
2024-07-18 10:41:48 +0600

    largest among three numbers

---
+++ python_100_programs/largest_three_num.py
@@ -0,0 +1,7 @@
+a = float(input("Enter the first number: "))
+b = float(input("Enter the first number: "))
+c = float(input("Enter the first number: "))
+
+max_num = max(a, b, c)
+
+print("Largest Number: ", max_num)
