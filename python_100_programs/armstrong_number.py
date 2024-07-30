2ab746c2e954f409b054033f8cd0023b58b522f6
SheikhULAB <tosibalaptop86@gmail.com>
2024-07-30 20:28:21 +0600

    armstrong number

---
+++ python_100_programs/armstrong_number.py
@@ -0,0 +1,16 @@
+def is_armstrong(n):
+    order = len(str(n))
+    temp = n
+    sum = 0
+    while temp > 0:
+        digit = temp % 10
+        sum += digit ** order
+        temp //= 10
+    return n == sum    
+
+number = int(input("Enter a number: "))
+if is_armstrong(number):
+    print("Armstrong number.")
+else:
+    print("Not an armstrong number.")    
+    
