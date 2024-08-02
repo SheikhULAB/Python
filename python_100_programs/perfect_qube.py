d44a92f9c0e5b22308044e0e95c53374ed39dd1e
SheikhULAB <tosibalaptop86@gmail.com>
2024-08-02 16:43:47 +0600

    perfect cube

---
+++ python_100_programs/perfect_qube.py
@@ -0,0 +1,10 @@
+def is_perfect_cube(n):
+    root = round(n ** (1/3))
+    return root ** 3 == n
+
+number = int(input("Enter a number: "))
+
+if is_perfect_cube(number):
+    print("Perfect cube")
+else:
+    print("Not a perfect cube")
