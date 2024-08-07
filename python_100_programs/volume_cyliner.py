869d0a2dbd58f46a21489ef48b770e3313c8be8f
SheikhULAB <tosibalaptop86@gmail.com>
2024-08-07 14:27:22 +0600

    calculate the volume of a cylinder

---
+++ python_100_programs/volume_cyliner.py
@@ -0,0 +1,8 @@
+import math
+
+radius = float(input("Enter the radius of the cylinder: "))
+height = float(input("Enter the height of the cyliner: "))
+
+volume = math.pi * radius * radius * height
+
+print("Volume of the cylinder: ", volume)
