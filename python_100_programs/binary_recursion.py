fe790e839429482bcfbd24ea2be52f97252344ee
SheikhULAB <tosibalaptop86@gmail.com>
2024-08-10 13:08:42 +0600

    binary search algorithm using recursion

---
+++ python_100_programs/binary_recursion.py
@@ -0,0 +1,21 @@
+def binary_search_recursive(arr, low, high, x):
+    if high >= low:
+        mid = (high + low) // 2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] > x:
+            return binary_search_recursive(arr, low, mid - 1, x)
+        else:
+            return binary_search_recursive(arr, mid + 1, high, x)
+    else:
+        return -1 
+    
+arr = [2,3,4,10,40]
+x = 10
+result = binary_search_recursive(arr, 0, len(arr) -1, x)
+
+if result != -1:
+    print(f"Element found at index {result}")
+else:
+    print("Element not found")        
+        
