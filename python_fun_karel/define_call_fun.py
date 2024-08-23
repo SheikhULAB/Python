3f9f48d51c3ae8521fe9884220ce1ea1489aa069
SheikhULAB <tosibalaptop86@gmail.com>
2024-08-23 10:46:06 +0600

    define and calling function

---
+++ python_fun_karel/define_call_fun.py
@@ -0,0 +1,14 @@
+# built in functions
+print("Hello")
+
+num_char = len("Hello")
+
+print(num_char)
+
+# user define functions
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+my_function()    
