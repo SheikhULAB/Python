ff29052d228fc8a9e8a40d595b98413a4b81dbb2
SheikhULAB <tosibalaptop86@gmail.com>
2024-08-24 17:10:37 +0600

    indentation

---
+++ python_fun_karel/indentation.py
@@ -0,0 +1,14 @@
+def my_function():
+    print("hello")
+    print("World")
+print("not in the function")
+
+def my_function():
+    if sky == "clear":
+        print("Blue")
+    elif sky == "cloudy":
+        print("grey")
+    print("Hello") 
+print("world")
+
+               
