b07fd8f15430e4ae17c3e828bcd1c7cff878a9a3
SheikhULAB <tosibalaptop86@gmail.com>
2024-08-24 17:20:53 +0600

    while loops

---
+++ python_fun_karel/while_loops.py
@@ -0,0 +1,16 @@
+#while something_is_true:
+    #Do something
+    #then do this
+    #then do this
+
+number_of_hurdles = 6
+
+while number_of_hurdles > 0:
+    jump()
+    number_of_hurdles -= 1
+    
+while not at_goal():
+    jump()
+    
+       
+    
