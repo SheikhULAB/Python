2ae660bfc7f21d8386ea11a7002f5038c806f4f7
SheikhULAB <tosibalaptop86@gmail.com>
2024-10-08 19:22:39 +0600

    multiple inputs

---
+++ functions_with_input/multiple_input.py
@@ -0,0 +1,6 @@
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+
+greet_with("Yeamin", "Dhaka")
+greet_with(location="New York", name="Ashraf")       
