942795f9c48eff90426c7f6a791e6c13cf39b421
SheikhULAB <tosibalaptop86@gmail.com>
2024-10-18 19:58:56 +0600

    dictionary deep dive

---
+++ dictionary/dictionary_deep_dive.py
@@ -0,0 +1,11 @@
+programming_dictionary = {
+    "bug": "An error in a program",
+    "Function": "A piece of code",
+    "Loop": "The action of doing something over and over again"
+    }
+
+print(programming_dictionary["bug"])
+
+programming_dictionary["if"]= "for if else in programming"
+
+print(programming_dictionary)
